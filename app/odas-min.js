2cc9eaa58556643c14a4a29b5718c247da30d3d2
Albin Rdz <ealbinu@gmail.com>
2022-02-03 11:50:06 -0600

    odasUpdates

---
+++ app/odas-min.js
@@ -0,0 +1,10 @@
+let app = new Vue({
+    el: '#odas',
+    data: {
+        start: 0,
+        norte: ['Bolaños', 'La Barca', 'Tuxpan'],
+        cienega: ['Villa Guerrero', 'San Gabriel'],
+        sur: ['Jamay','Colotlán']
+    }
+  })
+
